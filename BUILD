c744ed26f55097fbf9fc60ad4f696c436b78c579
notiom <947107282@qq.com>
2024-06-03 16:17:26 +0800

    talker

---
+++ BUILD
@@ -0,0 +1,56 @@
+load("@rules_cc//cc:defs.bzl", "cc_binary", "cc_library")
+
+# Define config settings for each folder
+config_setting(
+    name = "use_folder1",
+    define_values = {"folder": "01_hello_triangle"},
+)
+
+config_setting(
+    name = "use_folder2",
+    define_values = {"folder": "02_load_texture"},
+)
+
+config_setting(
+    name = "use_folder3",
+    define_values = {"folder": "03_transfrom"},
+)
+
+# Define the glad library
+cc_library(
+    name = "glad",
+    srcs = ["include/glad.c"],
+    hdrs = glob(["include/*.h"]),
+    includes = ["include"],  # This makes sure #include <glad/glad.h> works
+    visibility = ["//visibility:public"],
+)
+
+# Define the main binary with conditional srcs
+cc_binary(
+    name = "main",
+    srcs = select({
+        ":use_folder1": glob(["src/01_hello_triangle/*.cpp"]),
+        ":use_folder2": glob(["src/02_load_texture/*.cpp"]),
+        ":use_folder3": glob(["src/03_transfrom/*.cpp"]),
+        "//conditions:default": [],  # Default to empty if no folder is specified
+    }),
+    deps = [":glad"],
+    copts = [
+        "-Iinclude",  # Include the "include" directory for glad
+        "-I/usr/local/include",  # Include system path for glfw
+        "-fdiagnostics-color=always",
+    ],
+    linkopts = [
+        "-L/usr/local/lib",  # Link against system libraries
+        "-lGL",
+        "-lGLU",
+        "-lglut",
+        "-ldl",
+        "-lGLEW",
+        "-lglfw",
+        "-lX11",
+        "-lpthread",
+        "-lXrandr",
+    ],
+    visibility = ["//visibility:public"],
+)
--- include/shader.h
+++ include/shader.h
@@ -19,4 +19,88 @@ public:
     void Use();
 };
 
+#include "include/shader.h"
+
+Shader::Shader(const GLchar *vertexPath, const GLchar *fragmentPath)
+{
+     // 1. 从文件路径中获取顶点/片段着色器
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    // 保证ifstream可以抛出异常
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        //打开文件
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+
+        //读取文件的缓冲内容到流中
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        //关闭文件处理器
+        vShaderFile.close();
+        fShaderFile.close();
+        //转换流至GLchar数组
+        vertexCode = vShaderStream.str();
+        std::cout << "Read vertexshader code from " << vertexPath << ":\n" << vertexCode << std::endl; // Add this line for debugging
+        fragmentCode = fShaderStream.str();
+        std::cout << "Read fragmentshader code from " << fragmentPath << ":\n" << fragmentCode << std::endl; // Add this line for debugging
+    }
+    catch(const std::ifstream::failure e)
+    {
+        std::cerr <<  "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << e.what() << std::endl;
+    }
+    const GLchar* vShaderCode = vertexCode.c_str();
+    const GLchar* fShaderCode = fragmentCode.c_str();
+    // 2. 编译着色器
+    GLuint vertex,fragment;
+    GLint success;
+    GLchar infoLog[512];
+    //顶点着色器
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex,1,&vShaderCode,nullptr);
+    glCompileShader(vertex);
+    //检查编译是否成功
+    glGetShaderiv(vertex,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex,512,nullptr,infoLog);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //片段着色器
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment,1,&fShaderCode,nullptr);
+    glCompileShader(fragment);
+    glGetShaderiv(fragment,GL_COMPILE_STATUS,&success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragment,512,nullptr,infoLog);
+        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //着色器程序
+    this->Program = glCreateProgram();
+    glAttachShader(this->Program,vertex);
+    glAttachShader(this->Program,fragment);
+    glLinkProgram(this->Program);
+
+    glGetShaderiv(this->Program,GL_LINK_STATUS,&success);
+    if(!success)
+    {
+        glGetProgramInfoLog(this->Program,512,nullptr,infoLog);
+        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    //删除着色器
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+void Shader::Use()
+{
+    glUseProgram(this->Program);
+}
+
 #endif
